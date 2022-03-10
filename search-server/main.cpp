759e95b77b0b8378c0eacbcf94c53b9385223914
MamoruKuroji <98650017+MamoruKuroji@users.noreply.github.com>
2022-03-10 08:53:52 +0300

    Update main.cpp
    Исправление ошибок 2 спринта

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -26,16 +26,24 @@ void TestExcludeStopWordsFromAddedDocumentContent() {
 Разместите код остальных тестов здесь
 */
 void TestAddingDocumentContent() {
-    const int doc_id = 0;
-    const string document_text = "ухоженный пёс выразительные глаза и модный ошейник"s;
+    const int doc_id_0 = 0, doc_id_1 = 1, doc_id_2 = 2;
+    const string document_text_0 = "ухоженный пёс выразительные глаза и модный ошейник"s;
+    const string document_text_1 = "ухоженный кот выразительные глаза и модный ошейник"s;
+    const string document_text_2 = "кот и модный ошейник"s;
     const vector<int> doc_ratings = { 1, 2, 3 };
-    const DocumentStatus status = DocumentStatus::BANNED;
+    const DocumentStatus status = DocumentStatus::ACTUAL;
     SearchServer server;
     ASSERT_HINT(!server.GetDocumentCount(), "New search server has 0 documents");
-    server.AddDocument(doc_id, document_text, status, doc_ratings);
-
-    // Добавление документа увеличивает счетчик
+    server.AddDocument(doc_id_0, document_text_0, status, doc_ratings);
+    
     ASSERT_HINT(server.GetDocumentCount(), "New document not added");
+    
+    server.AddDocument(doc_id_1, document_text_1, status, doc_ratings);
+    server.AddDocument(doc_id_2, document_text_2, status, doc_ratings);
+    
+    ASSERT_EQUAL_HINT(server.GetDocumentCount(), 3 , "Incorrect adding several documents");
+
+    
 }
 
 void TestExludeMinusWordsFromDocument() {
@@ -78,6 +86,7 @@ void TestSortingDocumentContentByRelevance() {
     const Document& doc1 = found_docs[1];
     ASSERT_EQUAL_HINT(doc0.id, doc_id_1, "Incorrect searching by relevance");
     ASSERT_EQUAL_HINT(doc1.id, doc_id, "Incorrect searching by relevance");
+    ASSERT_EQUAL_HINT(doc0.relevance, doc1.relevance, "Relevance don't decrease");
 }
 
 void TestCalculateRatingOfDocumentContent() {    
@@ -111,10 +120,19 @@ void TestFiltrationDocumentContentWithUserPredicate() {
 void TestSearchDocumentContentWithAssignedStatus() {
     SearchServer server;
     const string document_text = "ухоженный пёс выразительные глаза и модный ошейник"s;
-    server.AddDocument(0, document_text, DocumentStatus::BANNED, { 5 , 6 , 7 });
+    server.AddDocument(0, document_text, DocumentStatus::ACTUAL, { 5 , 6 , 7 });
+    server.AddDocument(1, document_text, DocumentStatus::BANNED, { 5 , 6 , 7 });
+    server.AddDocument(2, document_text, DocumentStatus::IRRELEVANT, { 5 , 6 , 7 });
+    server.AddDocument(3, document_text, DocumentStatus::REMOVED, { 5 , 6 , 7 });
 
-    const auto found_docs = server.FindTopDocuments("пёс выразительные глаза"s, DocumentStatus::BANNED);
-    ASSERT_HINT(found_docs.size(), "Incorrect search whith assigned status");
+    const auto found_docs_ACTUAL = server.FindTopDocuments("пёс выразительные глаза"s, DocumentStatus::ACTUAL);
+    const auto found_docs_BANNED = server.FindTopDocuments("пёс выразительные глаза"s, DocumentStatus::BANNED);
+    const auto found_docs_IRRELEVANT = server.FindTopDocuments("пёс выразительные глаза"s, DocumentStatus::IRRELEVANT);
+    const auto found_docs_REMOVED = server.FindTopDocuments("пёс выразительные глаза"s, DocumentStatus::BANNED);
+    ASSERT_HINT(found_docs_ACTUAL.size(), "Incorrect search with ACTUAL status");
+    ASSERT_HINT(found_docs_BANNED.size(), "Incorrect search with BANNED status");
+    ASSERT_HINT(found_docs_IRRELEVANT.size(), "Incorrect search with IRRELEVANT status");
+    ASSERT_HINT(found_docs_REMOVED.size(), "Incorrect search with REMOVED status");
 }
 
 void TestCalculateRelevanceOfDocumentContent() {
@@ -124,7 +142,12 @@ void TestCalculateRelevanceOfDocumentContent() {
 
     const auto found_docs = server.FindTopDocuments("пёс выразительные глаза"s, DocumentStatus::BANNED);
     const Document& doc0 = found_docs[0];
-    ASSERT_EQUAL_HINT(doc0.relevance, ((log(1 * 1.0 / 1))*3), "Incorrect calculate relevance");
+    const double EPSILON = 1e-6;
+    bool IsEquel = false;
+    if((doc0.relevance - ((log(1 * 1.0 / 1))*3)) < EPSILON) {
+        IsEquel = true;
+    }
+    ASSERT_HINT(IsEquel, "Incorrect calculate relevance");
     
 }
 
